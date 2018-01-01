3cc672c46b2fa87ff79f31983d7f62312da8a2d9
idrobotov <idrobotov@localhost.localdomain>
2018-01-01 14:44:54 +0300

    stage 2 fixes

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,4 +1,4 @@
-#!/usr/bin/node
+#!/usr/bin/env node
 import askName from '..';
 
 console.log('Welcome to the Brain Games!');
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import readlineSync from 'readline-sync';
 
 const askName = () => {
-  const actual = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${actual}!`);
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
 };
 
 export default askName;
