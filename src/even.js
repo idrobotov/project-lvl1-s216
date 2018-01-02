8fd36946a3b50a4a984a1725638947b63812628f
idrobotov <idrobotov@localhost.localdomain>
2018-01-02 09:22:35 +0300

    logic improvements

--- src/even.js
+++ src/even.js
@@ -9,26 +9,27 @@ console.log('Answer "yes" if number even otherwise answer "no".\n');
 const userName = askName();
 
 const questionNum = 3; // number of questions in the game
-let times = questionNum;
 
 const evenGame = () => {
-  const num = getRandomInt(1, 20);
-  console.log(`Question: ${num}`);
-  const userAnswer = readlineSync.question('Your answer: ');
+  let times = questionNum;
+  while (times !== 0) {
+    const num = getRandomInt(1, 20);
+    console.log(`Question: ${num}`);
+    const userAnswer = readlineSync.question('Your answer: ');
 
-  if ((userAnswer === 'yes' && modCheck(num)) || (userAnswer === 'no' && !modCheck(num))) {
-    console.log('Correct!');
-  } else {
-    console.log('\'yes\' is wrong answer ;(. Correct answer was \'no\'.\n');
-    console.log(`Let's try again, ${userName}!`);
-    times = questionNum;
+    if ((userAnswer === 'yes' && modCheck(num)) || (userAnswer === 'no' && !modCheck(num))) {
+      console.log('Correct!');
+      times -= 1;
+    } else {
+      if (userAnswer === 'yes') { console.log('\'yes\' is wrong answer ;(. Correct answer was \'no\'.\n'); }
+      if (userAnswer === 'no') { console.log('\'no\' is wrong answer ;(. Correct answer was \'yes\'.\n'); }
+      console.log(`Let's try again, ${userName}!`);
+      times = questionNum;
+    }
+  }
+  if (times === 0) {
+    console.log(`Congratulations, ${userName}!`);
   }
 };
 
-while (times !== 0) {
-  evenGame();
-  times -= 1;
-}
-if (times === 0) {
-  console.log(`Congratulations, ${userName}!`);
-}
+export default evenGame;
