92aeca447715be7fc7fa6ef53da3ea3ebc9ccb48
idrobotov <ivan.drobotoff@yandex.ru>
2018-01-04 16:43:44 +0300

    structure improvement calc

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,40 +1,20 @@
 import { cons, car, cdr } from 'hexlet-pairs';
-import readlineSync from 'readline-sync';
-import askName from '../';
+import runGame from '..';
 import getRandomInt from '../getRandomInt';
 import getRandomOperator from '../getRandomOperator';
 import calcOperations from '../calcOperations';
 
-console.log('Welcome to the Brain Games!');
-console.log('What is the result of the expression?\n');
-
-const userName = askName();
-
-const questionNum = 3; // number of questions in the game
-
 const calcGame = () => {
-  let times = questionNum;
-
-  while (times !== 0) {
+  const description = 'What is the result of the expression?\n';
+  const calculate = () => {
     const pair = cons(getRandomInt(1, 50), getRandomInt(1, 50));
     const operator = getRandomOperator();
     const expected = calcOperations([car(pair), cdr(pair)], operator);
+    const question = `${car(pair)} ${operator} ${cdr(pair)}`;
+    return cons(question, expected);
+  };
 
-    console.log(`Question: ${car(pair)} ${operator} ${cdr(pair)}`);
-    const userAnswer = readlineSync.question('Your answer: ');
-
-    if (userAnswer === `${expected}`) {
-      console.log('Correct!');
-      times -= 1;
-    } else {
-      console.log(`It's wrong answer. Let's try again, ${userName}!`);
-      times = questionNum;
-    }
-  }
-
-  if (times === 0) {
-    console.log(`Congratulations, ${userName}!`);
-  }
+  runGame(description, calculate);
 };
 
 export default calcGame;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import readlineSync from 'readline-sync';
+import { car, cdr } from 'hexlet-pairs';
+
+const questionNum = 3; // number of questions in the game
 
 const askName = () => {
   const userName = readlineSync.question('May I have your name? ');
@@ -6,4 +9,28 @@ const askName = () => {
   return userName;
 };
 
-export default askName;
+const runGame = (description, game) => {
+  console.log('Welcome to the Brain Games!');
+  console.log(description);
+
+  const userName = askName();
+  let times = questionNum;
+  while (times !== 0) {
+    const gameState = game();
+    console.log(`Question: ${car(gameState)}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+
+    if (userAnswer === `${cdr(gameState)}`) {
+      console.log('Correct!');
+      times -= 1;
+    } else {
+      console.log(`It's wrong answer. Let's try again, ${userName}!`);
+      times = questionNum;
+    }
+  }
+  if (times === 0) {
+    console.log(`Congratulations, ${userName}!`);
+  }
+};
+
+export default runGame;
