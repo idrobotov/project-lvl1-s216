6f2eb2f60bc0ec0e805d9413df3b0522ce1f9080
idrobotov <ivan.drobotoff@yandex.ru>
2018-01-05 00:04:21 +0300

    gcd game structure improvement

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,38 +1,21 @@
 import { cons, car, cdr } from 'hexlet-pairs';
-import readlineSync from 'readline-sync';
-import askName from '../';
+import runGame from '..';
 import getRandomInt from '../getRandomInt';
-import getGcd from '../getGcd';
 
-console.log('Welcome to the Brain Games!');
-console.log('Find the greatest common divisor of given numbers.\n');
-
-const userName = askName();
-
-const questionNum = 3; // number of questions in the game
+/** This function returns greatest common divisor  */
+const getGcd = (a, b) => (b ? getGcd(b, a % b) : a);
 
 const gcdGame = () => {
-  let times = questionNum;
+  const description = 'Find the greatest common divisor of given numbers.\n';
 
-  while (times !== 0) {
+  const gcdCalc = () => {
     const pair = cons(getRandomInt(1, 100), getRandomInt(1, 100));
     const expected = getGcd(car(pair), cdr(pair));
+    const question = `${car(pair)} ${cdr(pair)}`;
+    return cons(question, expected);
+  };
 
-    console.log(`Question: ${car(pair)} ${cdr(pair)}`);
-    const userAnswer = readlineSync.question('Your answer: ');
-
-    if (userAnswer === `${expected}`) {
-      console.log('Correct!');
-      times -= 1;
-    } else {
-      console.log(`It's wrong answer. Let's try again, ${userName}!`);
-      times = questionNum;
-    }
-  }
-
-  if (times === 0) {
-    console.log(`Congratulations, ${userName}!`);
-  }
+  runGame(description, gcdCalc);
 };
 
 export default gcdGame;
--- src/getGcd.js
+++
@@ -1,4 +0,0 @@
-// Returns greatest common divisor
-const getGcd = (a, b) => (b ? getGcd(b, a % b) : a);
-
-export default getGcd;
