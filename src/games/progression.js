b3f85834c57d4de63a9ab88537f5e83f1304bb07
idrobotov <ivan.drobotoff@yandex.ru>
2018-01-07 00:10:21 +0300

    refactoring stage 8, deleted arrays

--- src/games/progression.js
+++ src/games/progression.js
@@ -2,21 +2,24 @@ import { cons } from 'hexlet-pairs';
 import runGame from '..';
 import getRandomInt from '../getRandomInt';
 
+const findProgressionMember = (startNum, step, n) => {
+  if (n > 1) {
+    return startNum + (step * (n - 1));
+  }
+  return startNum;
+};
+
 const generateProgression = (startNum, step, hidedIndex, n) => {
-  let start = startNum;
-  const progression = [];
-
-  for (let i = 0; i < n; i += 1) {
+  let progression = '';
+  for (let i = 1; i <= n; i += 1) {
     if (i === hidedIndex) {
-      progression.push('..');
-      start += step;
+      progression += '.. ';
+    } else {
+      progression += `${findProgressionMember(startNum, step, i)} `;
     }
-
-    progression.push(start);
-    start += step;
   }
 
-  return progression.join(' ');
+  return progression;
 };
 
 const progressionGame = () => {
@@ -26,13 +29,10 @@ const progressionGame = () => {
     const startNum = getRandomInt(1, 1000);
     const step = getRandomInt(1, 20);
     const n = 10;
-    const hidedIndex = getRandomInt(0, n - 1);
+    const hidedIndex = getRandomInt(1, n);
 
-    const progression = generateProgression(startNum, step, hidedIndex, n);
-    const answer = startNum + (step * hidedIndex);
-
-    const question = `${progression}`;
-
+    const answer = findProgressionMember(startNum, step, hidedIndex);
+    const question = generateProgression(startNum, step, hidedIndex, n);
     return cons(question, answer);
   };
 
